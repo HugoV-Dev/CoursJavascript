5fff8ad94f5cb067a8659ab486012601deccea10
HugoV-Dev <hugovinson@aol.fr>
2020-06-05 12:44:42 +0200

    JS

---
+++ script.js
@@ -0,0 +1,20 @@
+var name = "Hugo";  // variable chaîne de caractères
+console.log(name);  // affichage dans la console
+
+var nameLength = name.length;
+console.log(nameLength);
+
+var x = 10;  // variable entier
+var y = 12.1;  // variable décimal
+
+var xStr = x.toString(); // transforme x en chaine de caractères
+var xInt = parseInt(xStr); // retransforme x en entier
+
+var yStr = y.toString(); 
+var yFloat = parseFloat(yStr); // retransforme y en décimal
+
+console.log(xStr);
+console.log(xInt);
+
+console.log(x+y); // affichage dans la console de l'addition des 2 variables
+
