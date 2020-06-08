d9a7ca1e61dacf6ee812c8e71543f7afa6e4df3a
HugoV-Dev <hugovinson@aol.fr>
2020-06-08 15:54:34 +0200

    js

--- script.js
+++ script.js
@@ -18,3 +18,30 @@ console.log(xInt);
 
 console.log(x+y); // affichage dans la console de l'addition des 2 variables
 
+var myString = "Hello world";
+var position = myString.indexOf("world");
+console.log(position); // affiche la position du mot world dans la chaine de caractères
+var myNewString = myString.replace("world","Hugo");
+console.log(myNewString);
+
+var string1 = "Hello";
+var string2 = "world";
+var newString = string1 +" " + string2;
+console.log(newString);
+
+var num1 = 10;
+var num2 = 15;
+var result = num1 * num2;
+console.log(result);
+
+var bool = (num1 == 10 && num2 == 11);
+console.log(bool);
+
+if(bool == true)
+{
+    console.log("C'est vrai")
+}
+else
+{
+    console.log("C'est faux")
+};
